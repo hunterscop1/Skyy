f5a90caca278b7a0a88c5fe3f91c44ee7132ca92
hunterscop1 <89689730+hunterscop1@users.noreply.github.com>
2024-02-03 20:28:38 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ if (y === "ELMIRA HALIZA SUKMAWAN") {
     document.getElementById("kirim").setAttribute("href", "https://hunterscop1.github.io/ultah/")
 }
 else if (y === "GLADYS FLORENTHA") {
-    document.getElementById("kirim").setAttribute("href", "/g/index.html")
+    document.getElementById("kirim").setAttribute("href", "https://hunterscop1.github.io/pulung/")
 }
 else{
     document.getElementById("kirim").setAttribute("href", "/salah/index.html")
