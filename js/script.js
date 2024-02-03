43e7673563227cfe59f9a748ec06c4b1fc7793c4
hunterscop1 <89689730+hunterscop1@users.noreply.github.com>
2024-02-03 20:15:10 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 
 function myFunction() {
     var x = document.getElementById("nama").value;
-if (x === "elmira haliza sukmawan") {
+    var y = x.toUpperCase()
+if (y === "ELMIRA HALIZA SUKMAWAN") {
     document.getElementById("kirim").setAttribute("href", "https://hunterscop1.github.io/ultah/")
 }
-else if (x === "gladys florentha") {
+else if (y === "GLADYS FLORENTHA") {
     document.getElementById("kirim").setAttribute("href", "/g/index.html")
 }
 else{
